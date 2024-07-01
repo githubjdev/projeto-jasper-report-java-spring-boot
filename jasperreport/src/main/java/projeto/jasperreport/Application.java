deced9daee18959459e16567e1947bcdaaf54a44
afgale <alex.fernando.egidio@gmail.com>
2024-07-01 20:39:02 -0300

    relatorio em mvc

--- jasperreport/src/main/java/projeto/jasperreport/Application.java
+++ jasperreport/src/main/java/projeto/jasperreport/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  *
@@ -16,7 +15,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = "projeto.*")
 @EnableJpaRepositories(basePackages = "projeto.repository")
 @EnableTransactionManagement
-@EnableWebMvc
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
--- jasperreport/src/main/java/projeto/jasperreport/controllers/ImpressaoReportController.java
+++ jasperreport/src/main/java/projeto/jasperreport/controllers/ImpressaoReportController.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import projeto.jasperreport.ReportUtil;
@@ -26,89 +27,109 @@ import projeto.service.UsuarioService;
 @SuppressWarnings("rawtypes")
 @Controller
 public class ImpressaoReportController {
-	
-	
+
 	@Autowired
 	private ReportUtil reportUtil;
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
 	
-    /*Para download em aquivo pdf*/
+
+	/* Para download em aquivo pdf */
 	@RequestMapping(value = "**/imprimirreport/nome/{nome}/salario_ini/{salario_ini}/salario_fim/{salario_fim}", method = RequestMethod.GET)
-	public void imprimirReport(@PathVariable("nome") String nome,
-			                  @PathVariable("salario_ini") Double salario_ini,
-			                  @PathVariable("salario_fim") Double salario_fim,
-			                  HttpServletRequest request,
-			                  HttpServletResponse response) throws Exception {
-		
+	public void imprimirReport(@PathVariable("nome") String nome, @PathVariable("salario_ini") Double salario_ini,
+			@PathVariable("salario_fim") Double salario_fim, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+
 		ServletContext context = request.getServletContext();
 		HashMap params = new HashMap();
 		List listDados = new ArrayList();
-		
-		listDados = montaDados(nome, salario_ini, salario_fim, listDados);
-		
+
+		listDados = montaDados(params, nome, salario_ini, salario_fim, listDados);
+
 		byte[] reportByte = reportUtil.gerarRelatorioByte(params, context, nome, listDados);
-		
+
 		response.setContentLength(reportByte.length);
 		response.setContentType("application/octet-stream");
 		response.setHeader("Content-Disposition", "attachment;filename=report.pdf");
-		
+
 		OutputStream outputStream = response.getOutputStream();
 		outputStream.write(reportByte);
 		outputStream.flush();
 		outputStream.close();
-		
+
 	}
-	
-	
-    /*Para download em aquivo pdf em base 64 para API REST*/
+
+	/* Para download em aquivo pdf em base 64 para API REST */
 	@ResponseBody
 	@RequestMapping(value = "**/imprimirReportBase64/nome/{nome}/salario_ini/{salario_ini}/salario_fim/{salario_fim}", method = RequestMethod.GET)
 	public ResponseEntity<String> imprimirReportBase64(@PathVariable("nome") String nome,
-			                  @PathVariable("salario_ini") Double salario_ini,
-			                  @PathVariable("salario_fim") Double salario_fim,
-			                  HttpServletRequest request,
-			                  HttpServletResponse response) throws Exception {
-		
+			@PathVariable("salario_ini") Double salario_ini, @PathVariable("salario_fim") Double salario_fim,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
+
 		ServletContext context = request.getServletContext();
 		HashMap params = new HashMap();
 		List listDados = new ArrayList();
-		
-		listDados = montaDados(nome, salario_ini, salario_fim, listDados);
-		
+
+		listDados = montaDados(params, nome, salario_ini, salario_fim, listDados);
+
 		byte[] reportByte = reportUtil.gerarRelatorioByte(params, context, nome, listDados);
-		
+
 		String base64 = "data:application/text;base64," + DatatypeConverter.printBase64Binary(reportByte);
-		
+
 		return new ResponseEntity<String>(base64, HttpStatus.OK);
-		
+
 	}
 
 
-	private List montaDados(String nome, Double salario_ini, Double salario_fim, List listDados) {
-		
-		if (nome.equals("relatorio-usuario-jrbcds") 
-				|| nome.equals("relatorio-usuario-agrupamento-jrbcds") 
+
+	@SuppressWarnings("unchecked")
+	private List montaDados(HashMap params, String nome, Double salario_ini, Double salario_fim, List listDados) {
+
+		if (nome.equals("relatorio-usuario-jrbcds") || nome.equals("relatorio-usuario-agrupamento-jrbcds")
 				|| nome.equals("relatorio-usuario-com-subreport-jrbcds")) {
-			
+
 			listDados = usuarioRepository.listbySalario(salario_ini, salario_fim);
-			
-		}else if (nome.equals("relatorio-usuario-grafico-pizza-jrbcds") 
-				|| nome.equals("relatorio_usuario-barra-chart-jrbcds")
-				|| nome.equals("relatorio_usuario_tabela-jrbcds")){
-			
+
+		} else if (nome.equals("relatorio-usuario-grafico-pizza-jrbcds")
+				|| nome.equals("relatorio_usuario-barra-chart-jrbcds")) {
+
 			listDados = usuarioService.listUserGraficoPizzaAndBar();
-			
+
+		} else if (nome.equals("relatorio_usuario_tabela-jrbcds")) {
+		
+			params.put("lista_data_set_user", usuarioService.listUserGraficoPizzaAndBar());
+			listDados = usuarioRepository.findAll();
+	    
 		}else if (nome.equals("relatorio_usuario_crosstable-jrbcds")) {
-			
+
 			listDados = usuarioService.listUserCrossBar();
-			
+
 		}
 		return listDados;
 	}
+	
+	@RequestMapping(value = "/imprime-report-mvc", method =  RequestMethod.GET)
+	public String imprimeReportMvc(){
+		
+		return "imprime-report-mvc.html";
+	}
+	
+	@RequestMapping(value = "/imprimirReportMvc", method = RequestMethod.POST)
+	public String imprimirReportMvc(@RequestParam("nome") String nome,
+									@RequestParam("salario_ini") Double salario_ini,
+									@RequestParam("salario_fim") Double salario_fim,
+									HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		this.imprimirReport(nome, salario_ini, salario_fim, request, response);
+		
+		return "redirect:/" + imprimeReportMvc();
+		
+		
+	}
+	
 
 }
